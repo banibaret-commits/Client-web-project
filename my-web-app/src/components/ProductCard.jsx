f4897c8d7ac72ffe0d51219b3deb705e4a385724
Pocky <itselishalukibi@gmail.com>
2025-12-05 14:14:25 -0500

    I created code for the ProductCard

---
+++ my-web-app/src/components/ProductCard.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+const ProductCard = ({ product }) => {
+  const { addToCart } = useContext(CartContext);
+  return (
+    <div className="card h-100">
+      {" "}
+      <img src={product.thumbnail} className="card-img-top" />{" "}
+      <div className="card-body">
+        {" "}
+        <h5>{product.title}</h5>{" "}
+        <p className="text-primary">${product.price}</p>{" "}
+        <Link
+          to={`/product/${product.id}`}
+          className="btn btn-sm btn-outline-primary"
+        >
+          {" "}
+          View Details{" "}
+        </Link>{" "}
+        <button
+          className="btn btn-sm btn-success ms-2"
+          onClick={() => addToCart(product)}
+        >
+          {" "}
+          Add to Cart{" "}
+        </button>{" "}
+      </div>{" "}
+    </div>
+  );
+};
+export default ProductCard;
