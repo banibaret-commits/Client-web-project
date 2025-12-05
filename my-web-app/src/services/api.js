487ab566d34775f74c4e541eb170a55651b98d7e
Pocky <itselishalukibi@gmail.com>
2025-12-05 16:22:55 -0500

    To make the code work

--- my-web-app/src/services/api.js
+++ my-web-app/src/services/api.js
@@ -1,3 +1,5 @@
-const BASE = "https://dummyjson.com"; 
-export const getProducts = async () => fetch(`${BASE}/products?limit=100`).then(res => res.json()); 
-export const getProduct = async (id) => fetch(`${BASE}/products/${id}`).then(res => res.json());
+const BASE = "https://dummyjson.com";
+export const getProducts = async () =>
+  fetch(`${BASE}/products?limit=100`).then((res) => res.json());
+export const getProduct = async (id) =>
+  fetch(`${BASE}/products/${id}`).then((res) => res.json());
