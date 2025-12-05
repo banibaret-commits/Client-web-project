6a200f1de6c6d813fb80ffc7e067150c8685c8f9
Pocky <itselishalukibi@gmail.com>
2025-12-05 17:16:19 -0500

    This me

--- my-web-app/src/components/Navbar.jsx
+++ my-web-app/src/components/Navbar.jsx
@@ -6,14 +6,18 @@ const Navbar = () => {
   const { totalItems } = useContext(CartContext);
 
   return (
-    <nav className="navbar">
-      <Link className="navbar-brand text-white" to="/">
-        The Lady's Market
-      </Link>
+    <nav className="navbar navbar-dark bg-dark px-4">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        {/* Left side */}
+        <Link className="navbar-brand" to="/">
+          The Lady's Market
+        </Link>
 
-      <Link className="btn btn-outline-light cart-btn" to="/cart">
-        Cart ({totalItems})
-      </Link>
+        {/* Right side */}
+        <Link className="btn btn-outline-light" to="/cart">
+          Cart ({totalItems})
+        </Link>
+      </div>
     </nav>
   );
 };
